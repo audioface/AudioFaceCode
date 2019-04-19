cd6413cbcd4561beab2dd85c621241f5c31c82b0
vivianzzhu <tongyu@usc.edu>
2019-04-18 17:21:45 -0700

    clean

--- BackEnd-AudioFace/README.md
+++
@@ -1,1 +0,0 @@
-# audioface-backend
--- FrontEnd-AudioFace/global.js
+++
@@ -1,5 +0,0 @@
-global.logoColor = "#D25830";
-global.background = "#FDFAF3";
-global.themeColor = "#F78C74";
-global.headerHeight = 80;
-global.placeholder = "#B8B1AF";
